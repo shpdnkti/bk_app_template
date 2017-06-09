5af9489fb3b8e768297ccad70e16b468abbc81bf
bluekingassistant <3253845544@qq.com>
2017-06-09 11:31:02 +0800

    Create settings_development.py

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -12,9 +12,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
-        'PASSWORD': 'bk@321',                        # 你的数据库password
-        'HOST': '10.135.72.32',                   # 开发的时候，使用localhost
+          'USER': 'root',                        # 你的数据库user
+         'PASSWORD': '',                        # 你的数据库password
+        'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
+         'PASSWORD': 'bk@321',                        # 你的数据库password
+         'HOST': '10.135.72.32',                  # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
